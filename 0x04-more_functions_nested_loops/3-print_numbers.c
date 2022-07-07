487acc078a3f11d4f54c540ccac3dfd3144bd257
Shadrack Kiprono <vampeat@gmail.com>
2022-07-07 12:57:41 -0700

    print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers 0 to 9.
+ * Return: nil
+ */
+void print_numbers(void)
+{
+
+	int c;
+
+	for (c 30; c <= 39; c++)
+	{
+		printf("%c\n", c);
+	}
+	printf("\n");
+}
