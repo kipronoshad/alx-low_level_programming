4f088ca3c5d7d9e7235bcfdefbe7e57ea4a9fad8
Shadrack Kiprono <vampeat@gmail.com>
2022-08-04 21:52:35 -0700

    sum issue

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,19 +11,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
-
-	va_start(nums, n);
-
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-
-	va_end(nums);
-
 	if (n == 0)
+	{
 		return (0);
+	}
 	else
-		return (sum + n);
+	{
+		va_list nums;
+		unsigned int index, sum = 0;
+
+		va_start(nums, n);
+
+		for (index = 0; index < n; index++)
+			sum += va_arg(nums, int);
+
+			va_end(nums);
+			return (sum);
+	}
 
 }
